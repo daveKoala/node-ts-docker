f468e8ec315abc0056a518f4cb4a75a82da084d3
davidclare <mr.d.clare@gmail.com>
2020-02-07 16:46:21 +0000

    house keeping

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import { starwarsRouter } from "./services/starwars/starwars.router";
 
-// Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
-const PORT_REDIS = process.env.PORT || "6379";
-
-// App
 const app = express();
-// const redis_client = redis.createClient("redis://cache");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,13 +14,10 @@ app.use(bodyParser.json());
 // Routes
 app.use("/starwars", starwarsRouter);
 
+// Test
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World......");
 });
-
-
-
-
 
 app.listen(PORT, HOST);
 console.log(`Running on: //${ HOST }:${ PORT }`);
