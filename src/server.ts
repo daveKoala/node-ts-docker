d95185da113cf1b3d361b0cb9ded4d4514d472a9
Dave Clare <david.clare@gmail.com>
2020-03-12 16:49:08 +0000

    addes federated server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import { cache } from "./lib/redis";
 
 import { starwarsRouter } from "./services/starwars/starwars.router";
 
+import { ApolloServer } from "apollo-server-express";
+import { ApolloGateway } from "@apollo/gateway";
+
 const PORT = 8080;
 const HOST = "0.0.0.0";
 const app = express();
@@ -23,5 +26,25 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World.");
 });
 
+try {
+  const gateway = new ApolloGateway({
+    serviceList: [
+      { name: 'users', url: 'http://localhost:61105/graphql' },
+      // more services
+    ],
+  });
+
+  const graphQLServer = new ApolloServer({
+    gateway,
+
+    // Disable subscriptions (not currently supported with ApolloGateway)
+    subscriptions: false,
+  });
+
+  const options = { app, path: "/graphql" };
+  graphQLServer.applyMiddleware(options);
+} catch (err) {
+  console.error(err.message);
+}
 app.listen(PORT, HOST);
 console.log(`Running on: //${HOST}:${PORT}`);
